4d4124cc488074f64cdc3f58d1fea27735c48855
Sibi Siddharthan <sibisiddharthan.github@gmail.com>
2025-08-29 03:37:46 +0530

    Add prime sieve

--- nt.cpp
+++ nt.cpp
@@ -1,4 +1,7 @@
 #include <cstdint>
+#include <vector>
+
+using namespace std;
 
 uint64_t gcd(uint64_t a, uint64_t b)
 {
@@ -18,6 +21,30 @@ uint64_t gcd(uint64_t a, uint64_t b)
 		t = a;
 		a = b;
 		b = t;
+	}
+
+	return a;
+}
+
+vector<uint64_t> prime_sieve(uint64_t n)
+{
+	vector<uint8_t> v(n + 1, 0);
+	vector<uint64_t> a;
+
+	for (uint64_t i = 2; i < n + 1; ++i)
+	{
+		if (v[i] == 0)
+		{
+			uint64_t j = 2;
+
+			a.push_back(i);
+
+			while ((i * j) <= n)
+			{
+				v[i * j] = 1;
+				j++;
+			}
+		}
 	}
 
 	return a;
