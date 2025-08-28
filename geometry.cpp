692605747abd0281e2a9fef8083bfeeadff09d17
Sibi Siddharthan <sibisiddharthan.github@gmail.com>
2025-08-28 14:37:09 +0530

    Add 2d euclidean distance

--- geometry.cpp
+++ geometry.cpp
@@ -1,4 +1,5 @@
 #include <cstdint>
+#include <cmath>
 
 #define ABS_SUB(x, y) ((x) < (y) ? ((y) - (x)) : ((x) - (y)))
 
@@ -12,4 +13,16 @@ template <typename T>
 T manhattan_distance(point<T> *p1, point<T> *p2)
 {
 	return ABS_SUB(p1->x, p2->x) + ABS_SUB(p1->y, p2->y);
+}
+
+template <typename T>
+T euclid_distance_sq(point<T> *p1, point<T> *p2)
+{
+	return (ABS_SUB(p1->x, p2->x) * ABS_SUB(p1->x, p2->x)) + (ABS_SUB(p1->y, p2->y) * ABS_SUB(p1->y, p2->y));
+}
+
+template <typename T>
+T euclid_distance(point<T> *p1, point<T> *p2)
+{
+	return sqrt(((p1->x - p2->x) * (p1->x - p2->x)) + ((p1->y - p2->y) * (p1->y - p2->y)));
 }
