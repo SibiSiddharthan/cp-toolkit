8ec17b7aa47e4395256087b92d740e4e0ccdce6c
Sibi Siddharthan <sibisiddharthan.github@gmail.com>
2025-09-15 21:18:10 +0530

    Add backward prefix sum routine

--- extra.cpp
+++ extra.cpp
@@ -20,6 +20,23 @@ vector<T> forward_prefix_sums(vector<T> &v)
 }
 
 template <typename T>
+vector<T> backward_prefix_sums(vector<T> &v)
+{
+	size_t n = v.size();
+	T sum = 0;
+
+	vector<T> p(n, 0);
+
+	for (size_t i = n; i != 0; --i)
+	{
+		sum += v[i - 1];
+		p[i - 1] = sum;
+	}
+
+	return p;
+}
+
+template <typename T>
 T range_sum(vector<T> &prefix_sums, size_t left, size_t right)
 {
 	return prefix_sums[right] - (left == 0 ? 0 : prefix_sums[left - 1]);
