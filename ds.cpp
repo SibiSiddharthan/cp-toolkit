40e30a194b734abf2b19ef9a0e3b76165d62bdbd
Sibi Siddharthan <sibisiddharthan.github@gmail.com>
2025-10-13 04:59:14 +0530

    Add fenwick tree

--- ds.cpp
+++ ds.cpp
@@ -45,3 +45,68 @@ T sparse_query(vector<vector<T>> &sparse, size_t left, size_t right)
 
 	return a;
 }
+
+template <typename T>
+struct fenwick_tree
+{
+	vector<T> tree;
+	size_t size = 0;
+
+	fenwick_tree(const vector<T> &value)
+	{
+		this->tree = value;
+		this->size = value.size();
+
+		for (size_t i = 0; i < size; ++i)
+		{
+			size_t above = i + ((size_t)1 << __builtin_ctzll(i + 1));
+
+			if (above < size)
+			{
+				this->tree[above] = this->tree[above] + this->tree[i];
+			}
+		}
+	}
+
+	T psum(size_t index)
+	{
+		T sum = 0;
+		index += 1;
+
+		while (index != 0)
+		{
+			sum = sum + tree[index - 1];
+			index -= (size_t)1 << __builtin_ctzll(index);
+		}
+
+		return sum;
+	}
+
+	T sum(size_t left, size_t right)
+	{
+		if (left >= this->size || right >= this->size)
+		{
+			return 0;
+		}
+
+		if (left == 0)
+		{
+			return this->psum(right);
+		}
+		else
+		{
+			return this->psum(right) - this->psum(left - 1);
+		}
+	}
+
+	void update(size_t index, T value)
+	{
+		T old = this->sum(index, index);
+
+		while (index < size)
+		{
+			this->tree[index] = (this->tree[index] + value) - old;
+			index += (size_t)1 << __builtin_ctzll(index + 1);
+		}
+	}
+};
