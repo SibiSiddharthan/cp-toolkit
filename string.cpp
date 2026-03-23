a93d89483b32b43de73e58c13b9223b53f661634
Sibi Siddharthan <sibisiddharthan.github@gmail.com>
2026-03-23 19:26:23 +0530

    Add z algorithm

--- string.cpp
+++ string.cpp
@@ -3,6 +3,9 @@
 
 using namespace std;
 
+#define MAX(a, b) (((a) > (b)) ? (a) : (b))
+#define MIN(a, b) (((a) < (b)) ? (a) : (b))
+
 uint64_t mods[2] = {4294967279, 4294967291};
 uint64_t primes[4] = {73, 137, 101, 103};
 
@@ -81,4 +84,33 @@ vector<uint32_t> kmp_search_all(vector<uint32_t> &lps, const string &pattern, co
 	}
 
 	return matches;
+}
+
+vector<uint32_t> z_prefix(const string &str)
+{
+	uint32_t size = str.size();
+	uint32_t left = 0, right = 0;
+
+	vector<uint32_t> prefix(size, 0);
+
+	for (uint32_t i = 1; i < size; ++i)
+	{
+		if (i < right)
+		{
+			prefix[i] = MIN(right - i, prefix[i - left]);
+		}
+
+		while (i + prefix[i] < size && str[prefix[i]] == str[i + prefix[i]])
+		{
+			prefix[i] += 1;
+		}
+
+		if (i + prefix[i] > right)
+		{
+			left = i;
+			right = i + prefix[i];
+		}
+	}
+
+	return prefix;
 }
