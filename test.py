7750c65953de6b356acf51b9a343d93fcb7f2fac
Sibi Siddharthan <sibisiddharthan.github@gmail.com>
2026-05-08 13:00:07 +0530

    Update test generation

--- test.py
+++ test.py
@@ -5,25 +5,29 @@ def generate_random_ints(count: int, lower: int, upper: int, sum: int = None, ex
 
     result = []
     current = 0
+
+    lower = int(lower)
+    upper = int(upper)
+
     base = lower
-    range = upper - lower
+    max = upper - lower
 
     if sum != None:
         if base != 0:
-            range = sum // (count * base)
+            max = sum // (count * base)
 
     for _ in range(count):
 
         if sum != None:
-            range -= current
+            max -= current
 
         if exclude is None:
-            value = random.randint(0, range)
+            value = random.randint(0, max)
             current += value
             result.append(base + value)
         else:
             while True:
-                value = random.randint(0, range)
+                value = random.randint(0, max)
 
                 if (base + value) not in exclude:
                     current += value
@@ -35,19 +39,49 @@ def generate_random_ints(count: int, lower: int, upper: int, sum: int = None, ex
 
 def generate_random_floats(count: int, lower: float, upper: float):
 
-    return [random.uniform(lower, upper) for _ in range(count)]
+    return [random.uniform(lower, upper) for _ in range(int(count))]
+
+
+def generate_random_range(lower: int, upper: int, strict: bool = False):
+
+    left = 0
+    right = 0
+
+    lower = int(lower)
+    upper = int(upper)
+
+    if strict:
+        left = random.randint(lower, upper - 1)
+        right = random.randint(left + 1, upper)
+    else:
+        left = random.randint(lower, upper)
+        right = random.randint(left, upper)
+
+    return (left, right)
 
 
 def generate_random_string(size: int, chars: list[str] = "abcdefghijklmnopqrstuvwxyz"):
-    return ''.join(random.choice(chars) for _ in range(size))
+    return ''.join(random.choice(chars) for _ in range(int(size)))
 
 
-def generate_random_grid(height, width, chars: list[str] = ".#"):
+def generate_random_grid(height: int, width: int, chars: list[str] = ".#"):
 
     grid = []
 
-    for _ in range(height):
-        grid.append(''.join(random.choice(chars) for _ in range(width)))
+    for _ in range(int(height)):
+        grid.append(''.join(random.choice(chars) for _ in range(int(width))))
 
     return grid
 
+
+TESTS = 1
+print(TESTS)
+
+
+def generate_testcase():
+    n = int(0)
+    q = int(0)
+
+
+for _ in range(TESTS):
+    generate_testcase()
