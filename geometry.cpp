84adf81c2f7fa39b8278ec56b7a93653ca7daa3a
Sibi Siddharthan <sibisiddharthan.github@gmail.com>
2026-06-01 15:54:33 +0530

    Update point

--- geometry.cpp
+++ geometry.cpp
@@ -7,32 +7,32 @@ struct point
 };
 
 template <typename T>
-T manhattan_distance(point<T> *p1, point<T> *p2)
+T manhattan_distance(const point<T> &p1, const point<T> &p2)
 {
-	return DIFF(p1->x, p2->x) + DIFF(p1->y, p2->y);
+	return DIFF(p1.x, p2.x) + DIFF(p1.y, p2.y);
 }
 
 template <typename T>
-T euclid_distance_sq(point<T> *p1, point<T> *p2)
+T euclid_distance_sq(const point<T> &p1, const point<T> &p2)
 {
-	return (DIFF(p1->x, p2->x) * DIFF(p1->x, p2->x)) + (DIFF(p1->y, p2->y) * DIFF(p1->y, p2->y));
+	return (DIFF(p1.x, p2.x) * DIFF(p1.x, p2.x)) + (DIFF(p1.y, p2.y) * DIFF(p1.y, p2.y));
 }
 
 template <typename T>
-T euclid_distance(point<T> *p1, point<T> *p2)
+T euclid_distance(const point<T> &p1, const point<T> &p2)
 {
-	return sqrt(((p1->x - p2->x) * (p1->x - p2->x)) + ((p1->y - p2->y) * (p1->y - p2->y)));
+	return sqrt((DIFF(p1.x, p2.x) * DIFF(p1.x, p2.x)) + (DIFF(p1.y, p2.y) * DIFF(p1.y, p2.y)));
 }
 
 template <typename T>
 struct line
 {
 	// ax + by = c
-	T a, b, c; 
+	T a, b, c;
 };
 
 template <typename T>
-point<T> intersect(const line<T> &l1, const line<T> &l2)
+point<T> line_intersect(const line<T> &l1, const line<T> &l2)
 {
 	double dd = (l1.b * l2.a) - (l1.a * l2.b);
 	T x = ((l1.b * l2.c) - (l1.c * l2.b)) / dd;
