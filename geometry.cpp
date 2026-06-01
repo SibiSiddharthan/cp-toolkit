cb7094ea19ce849202ba5e1f592f96c71b3a74f1
Sibi Siddharthan <sibisiddharthan.github@gmail.com>
2026-06-01 15:52:48 +0530

    Add line stuff

--- geometry.cpp
+++ geometry.cpp
@@ -23,3 +23,20 @@ T euclid_distance(point<T> *p1, point<T> *p2)
 {
 	return sqrt(((p1->x - p2->x) * (p1->x - p2->x)) + ((p1->y - p2->y) * (p1->y - p2->y)));
 }
+
+template <typename T>
+struct line
+{
+	// ax + by = c
+	T a, b, c; 
+};
+
+template <typename T>
+point<T> intersect(const line<T> &l1, const line<T> &l2)
+{
+	double dd = (l1.b * l2.a) - (l1.a * l2.b);
+	T x = ((l1.b * l2.c) - (l1.c * l2.b)) / dd;
+	T y = ((l1.c * l2.a) - (l1.a * l2.c)) / dd;
+
+	return {x, y};
+}
